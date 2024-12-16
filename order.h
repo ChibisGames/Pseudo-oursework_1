62275ec08e80a4684b323ef2c3ba280b8b62a2dc
kirimusha <mariya.Kirichek2006@mail.ru>
2024-12-16 21:13:14 +0300

    добавление заказа

--- order.h
+++ order.h
@@ -1,22 +1,35 @@
-#pragma once
+// Data struct of Order and OrderDetail
+#ifndef ORDER_H
+#define ORDER_H
 
 #include "client.h"
 #include "order_detail.h"
+#define MAX 1828
 
+// order details attributes
+struct OrderDetail{
+    Product product;
+    int quantity;
+    double subtotal;
+};
 
-struct Order
-{
+// order attributes
+struct Order{
     int code;
-    Client order;
-    OrderDetail *detail;
-    int num;
+    Client client;
+    OrderDetail details[MAX];
+    int num_details;
+    double total;
+
+    Order():
+        num_details(0), total(0.0){}
 };
 
+// methods
+void menu_orders(int *opt, struct Client *lst_clients, struct Product *lst_products, struct Order *lst_orders);
+void insert_order(struct Order *ord, struct Client *lst_clients, struct Product *lst_products);
+int get_num_orders();
+void print_list_orders(struct Order *lst_orders);
+void print_order(struct Order ord);
 
-static int num_orders = 0;
-static int order_index = 0;
-
-void menu_orders(int *opt, struct Client *list_clients, struct Book *list_books, struct Order *list_order)
-{
-
-};
+#endif
